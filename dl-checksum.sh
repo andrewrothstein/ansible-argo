9669c6b54368d52856bb7f4d4bc51ffc4df688d4
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-08-21 23:57:17 -0400

    argo:3.1.8

---
+++ dl-checksum.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://github.com/argoproj/argo-workflows/releases/download
+APP=argo
+
+# https://github.com/argoproj/argo-workflows/releases/download/v3.1.8/argo-linux-amd64.gz
+
+dl()
+{
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local platform="$os-$arch"
+    local url="${MIRROR}/v${ver}/${APP}-${platform}.gz.sha256"
+
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(curl -qsSL $url)
+}
+
+dlver () {
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver darwin amd64
+    dl $ver linux amd64
+    dl $ver linux arm64
+    dl $ver linux ppc64le
+    dl $ver linux s390x
+    dl $ver windows amd64
+}
+
+dlver ${1:-3.1.8}
